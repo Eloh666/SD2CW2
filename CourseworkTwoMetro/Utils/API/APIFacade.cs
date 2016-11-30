4179494eb36bc0406ef7e63f5b569ac2994f2b57
DESKTOP-9PESPAJ\Eloh <eloh666@gmail.com>
2016-11-30 21:00:46 +0000

    JWT != Bearer

--- CourseworkTwoMetro/Utils/API/APIFacade.cs
+++ CourseworkTwoMetro/Utils/API/APIFacade.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using CourseworkTwoMetro.Models;
+using CourseworkTwoMetro.Models.Extras;
 using CourseworkTwoMetro.Utils.JSONUtils;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -13,17 +15,33 @@ namespace CourseworkTwoMetro.Utils.API
 {
     public class ApiFacade
     {
+        //private const string Host = "https://coursework2api.herokuapp.com/";
+        private const string Host = "http://127.0.0.1:5000";
+
         private static HttpClient _client;
         private static string _jwt;
 
+
         public static void InitialiseApi()
         {
-            _client = new HttpClient { BaseAddress = new Uri("http://127.0.0.1:5000/") };
+            _client = new HttpClient { BaseAddress = new Uri(Host) };
             _client.DefaultRequestHeaders.Accept.Clear();
             _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        public static async Task<String> GetData(string path)
+        public static async Task<ObservableCollection<Customer>> GetCustomers()
+        {
+            var json = await GetData("/customer");
+            return JsonConvert.DeserializeObject<ObservableCollection<Customer>>(json);
+        }
+
+        public static async Task<ObservableCollection<Booking>> GetBookings()
+        {
+            var json = await GetData("/booking");
+            return JsonConvert.DeserializeObject<ObservableCollection<Booking>>(json);
+        }
+
+        public static async Task<string> GetData(string path)
         {
             HttpResponseMessage response = await _client.GetAsync(path);
             if (response.IsSuccessStatusCode)
@@ -36,18 +54,89 @@ namespace CourseworkTwoMetro.Utils.API
             }
         }
 
-
-        public static async Task<List<Customer>> GetCustomers()
+        public static async Task<bool> DeleteBooking(Booking booking)
         {
-            var json = await GetData("/customer");
-            return JsonConvert.DeserializeObject<List<Customer>>(json);
+            string json = MyJsonSerializer.Serialize(booking);
+            return await DeleteItem(json, "/booking");
         }
 
-        public static async Task<List<Booking>> GetBookings()
+        public static async Task<bool> DeleteCustomer(Customer customer)
         {
-            var json = await GetData("/booking");
-            Console.WriteLine(json);
-            return JsonConvert.DeserializeObject<List<Booking>>(json);
+            string json = MyJsonSerializer.Serialize(customer);
+            return await DeleteItem(json, "/customer");
+        }
+
+        public static async Task<bool> DeleteItem(string json, string path)
+        {
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            try
+            {
+                HttpRequestMessage request = new HttpRequestMessage { Content = content, Method = HttpMethod.Delete, RequestUri = new Uri(Host + path) };
+                await _client.SendAsync(request);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static async Task<Booking> SaveBooking(Booking booking, bool shouldPutInsteadOfPost)
+        {
+            string json = MyJsonSerializer.Serialize(booking);
+            var savedCustomerData = shouldPutInsteadOfPost ? await PutData(json, "/booking") : await PostData(json, "/booking"); ;
+            try
+            {
+                return JsonConvert.DeserializeObject<Booking>(savedCustomerData);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static async Task<Customer> SaveCustomer(Customer customer, bool shouldPutInsteadOfPost)
+        {
+            string json = MyJsonSerializer.Serialize(customer);
+            var savedCustomerData = shouldPutInsteadOfPost ? await PutData(json, "/customer") : await PostData(json, "/customer");;
+            try
+            {
+                return JsonConvert.DeserializeObject<Customer>(savedCustomerData);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static async Task<string> PostData(string json, string path)
+        {
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            try
+            {
+                var response = await _client.PostAsync(path, content);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch
+            {
+                return "An error has occurred when trying to save the data";
+            }
+        }
+
+        public static async Task<string> PutData(string json, string path)
+        {
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            try
+            {
+                var response = await _client.PutAsync(path, content);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch
+            {
+                return null;
+            }
         }
 
         public static async Task<bool> Login(User user)
@@ -61,8 +150,7 @@ namespace CourseworkTwoMetro.Utils.API
 
                 JObject jwt = JObject.Parse(await response.Content.ReadAsStringAsync());
                 _jwt = (string)jwt["access_token"];
-                _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _jwt);
-                Console.WriteLine(_jwt);
+                _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("JWT", _jwt);
                 return true;
             }
             catch
